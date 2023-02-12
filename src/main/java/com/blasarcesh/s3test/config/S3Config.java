57eda0e1e00bfb85dbd0e872fb9b65354b05f920
armandoB-A <blasarce@gnail.com>
2023-02-12 14:14:52 -0600

    deescubrí como ocular las credenciales (instalar aws tool kit y colocar las credenciales en el archivo de configuracion xd (leer documentacion))

--- src/main/java/com/blasarcesh/s3test/config/S3Config.java
+++ src/main/java/com/blasarcesh/s3test/config/S3Config.java
@@ -2,24 +2,29 @@ package com.blasarcesh.s3test.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfilesConfigFile;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.access_key_id}")
-    private String accesskeyId;
-    @Value("${aws.secret_access_key1}")
-    private String accessSecretKey;
     @Value("${aws.s3.region}")
     private String region;
+
+
     @Bean
     public AmazonS3 getS3Client() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accesskeyId, accessSecretKey);
+        ProfileCredentialsProvider profileCredentialsProvider =
+                new ProfileCredentialsProvider();
+        BasicAWSCredentials credentials = new BasicAWSCredentials(
+                profileCredentialsProvider.getCredentials().getAWSAccessKeyId(),
+                profileCredentialsProvider.getCredentials().getAWSSecretKey());
         return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
                 .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
     }
